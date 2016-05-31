d5011f667338a50eb807d3a0ed182e9086856cb5
Lahiru P Fernando <lahiru|christy|wariya>
2016-05-31 16:40:29 +0530

    wariya done left bar with eloquent

--- resources/views/partials/_leftbar.blade.php
+++ resources/views/partials/_leftbar.blade.php
@@ -1,19 +1,45 @@
-<ul class="ppost_nav" style="padding-top:15px; padding-left:10px;">
-            
 
+<?php
+
+  use App\Post; 
+  $posts = Post::all();
+?>
+<div>
+
+
+<?php
+  foreach ($posts as $post) {
+    # code...
+    echo $post->title;
+  }
+?>
+
+</div>
+
+
+
+<ul class="ppost_nav" style="padding-top:15px; padding-left:10px;">
           
-            <li>
+          
+              @foreach ($posts as $post)
+                      <li>
                           <div class="media" >
-                            <a class="media-left" href="single_page.html">
-                              <img src="http://lorempixel.com/70/70/" alt="img">
-                            </a>
-                            <div class="media-body">
-                              <a class="catg_title" href="single_page.html" style="text-align:justify;"></a>                    
-                            </div> 
-                            <div class="numberofviews2"><font color="#000000"><b><span class="glyphicon glyphicon-eye-open "></span> 5,253</b></font> Views</div>
-                            <div class="numberofviews2"><font color="#000000"><b><span class="glyphicon glyphicon-thumbs-up"></span> 5,253</b></font> Likes</div>
+                              <a class="media-left" href="single_page.html">
+                                  {{$post->title}}
+                              </a>
+                              <div class="media-body">
+                                <a class="catg_title" href="single_page.html" style="text-align:justify;">
+                                  {{$post->description}}
+                                </a>                    
+                              </div> 
+                              <div class="numberofviews2"><font color="#000000"><b><span class="glyphicon glyphicon-eye-open "></span> 5,253</b></font> Views</div>
+                              <div class="numberofviews2"><font color="#000000"><b><span class="glyphicon glyphicon-thumbs-up"></span> 5,253</b></font> Likes</div>
+                              <div class="timestamp">
+                                  {{$post->created_at}}
+                              </div>
                         </div>
-            </li>     
+            </li>  
+           @endforeach
              <div class="hr-primary" />  
                                       
-         </ul>
+</ul>
